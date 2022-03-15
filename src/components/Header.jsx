e60438a2328ff5dca5e6b3fe4a326ebd02965ed4
Астапчик Матвей <m.astapchik@sellwin.by>
2022-03-15 07:43:41 +0300

    new-lesson-13

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header_1'>
+            <img src='https://www.logodesign.net/images/nature-logo.png' />
+        </header>)
+}
+
+export default Header
