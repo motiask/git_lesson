0c0de17d639785deed0d2632b6c81e513c80cc30
Астапчик Матвей <m.astapchik@sellwin.by>
2022-03-17 21:08:33 +0300

    lesson 18

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import d from './Dialogs.module.css';
+
+const Dialogs = (props) => {
+    return(
+        <div>  
+            DIALOGS        
+        </div>
+    )
+}
+
+export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import p from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className={p.content_1}>
+        <div>
             <div>
                 <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
             </div>
