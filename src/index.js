03c3ed2614ecc54cdf61b32392d2193f30d37c1e
Астапчик Матвей <m.astapchik@sellwin.by>
2022-03-23 19:29:09 +0300

    lesson 37

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { addPost, PushPostText, subscribe } from './redux/state';
+
 
 {/*-32-2 вызов функции с добавлением новых данных
 addPost('новый текст');
@@ -14,7 +14,7 @@ let rerenderEntireTree_callBack = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             {/*-32-3 функцию addPost прокидывем через props*/}
-            <App state={state} addPost={addPost} PushPostText={PushPostText} />
+            <App state={state} addPost={store.addPost.bind(store)} PushPostText={store.PushPostText.bind(store)} />
         </React.StrictMode>,
         document.getElementById('root')
     );
@@ -23,11 +23,11 @@ let rerenderEntireTree_callBack = (state) => {
 
 {/*-33-1 Перерисовка страницы!*/ }
 console.log('начальный запус, рисуем страницу!');
-rerenderEntireTree_callBack(state);
+rerenderEntireTree_callBack(store.getState());
 
 {/*-35-2 передаем в state callback для вызова!*/}
 console.log('начальный запус, переопредляем функцию на callback!');
-subscribe(rerenderEntireTree_callBack);
+store.subscribe(rerenderEntireTree_callBack);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,63 +1,71 @@
-{/*-35-3 переопределяем функцию!!!*/}
-let rerenderEntireTree = () => {
-    console.log('начальный запус, далее будет переопределение!')
-}
-{/*Данные!*/ }
-
-let state = {
-    ProfilePage: {
-        postData: [
-            { id: '1', avatar: 'https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg', name: 'Мне еще 30 лет!', likeCount: 2 },
-            { id: '2', avatar: 'https://n1s2.starhit.ru/6a/46/ae/6a46aeed947a183d67d1bc48211151bf/480x496_0_2bbde84177c9ff1c2299a26a0f69f69c@480x496_0xac120003_4430520541578509619.jpg', name: 'А мне 40 лет', likeCount: 11 }
-        ],
-        newPostText: ''
+let store = {
+    _state: {
+        ProfilePage: {
+            postData: [
+                { id: '1', avatar: 'https://static.1tv.ru/uploads/photo/image/2/huge/4062_huge_876c41f50e.jpg', name: 'Мне еще 30 лет!', likeCount: 2 },
+                { id: '2', avatar: 'https://n1s2.starhit.ru/6a/46/ae/6a46aeed947a183d67d1bc48211151bf/480x496_0_2bbde84177c9ff1c2299a26a0f69f69c@480x496_0xac120003_4430520541578509619.jpg', name: 'А мне 40 лет', likeCount: 11 }
+            ],
+            newPostText: ''
+        },
+        DialogsPage: {
+            dialogsData: [
+                { id: '1', nameD: 'user 1' },
+                { id: '2', nameD: 'user 2' },
+                { id: '3', nameD: 'user 3' },
+                { id: '4', nameD: 'user 4' },
+                { id: '5', nameD: 'user 5' },
+                { id: '6', nameD: 'user 6' }
+            ],
+            messagesData: [
+                { id: '1', message: 'message 1' },
+                { id: '2', message: 'message 2' },
+                { id: '3', message: 'message 3' },
+                { id: '4', message: 'message 4' },
+                { id: '5', message: 'message 5' }
+            ]
+        }
     },
-    DialogsPage: {
-        dialogsData: [
-            { id: '1', nameD: 'user 1' },
-            { id: '2', nameD: 'user 2' },
-            { id: '3', nameD: 'user 3' },
-            { id: '4', nameD: 'user 4' },
-            { id: '5', nameD: 'user 5' },
-            { id: '6', nameD: 'user 6' }
-        ],
-        messagesData: [
-            { id: '1', message: 'message 1' },
-            { id: '2', message: 'message 2' },
-            { id: '3', message: 'message 3' },
-            { id: '4', message: 'message 4' },
-            { id: '5', message: 'message 5' }
-        ]
+
+    getState() {
+        return this._state;
+    },
+
+    /*-35-3 переопределяем функцию!!!*/
+    _callSubscribe() {
+        console.log('начальный запус, далее будет переопределение!')
+    },
+    /*Данные!*/
+
+    /*-35-1 избавление от цикличности CALLBACK*/
+    subscribe(observer) {
+        this._callSubscribe = observer;
+    },
+
+    /*-33-1 обновление текста в state после ввода на странице, для последующего отображения*/
+    PushPostText(newPostText) {
+        this._state.ProfilePage.newPostText = newPostText;
+        this._callSubscribe(this._state);
+    },
+
+    /*-32-1 добавление данных функция из BLL*/
+    addPost() {
+        let newPost = {
+            id: '3',
+            avatar: '',
+            name: this._state.ProfilePage.newPostText,
+            likeCount: 0
+        };
+        this._state.ProfilePage.newPostText = '';
+        this._state.ProfilePage.postData.push(newPost)
+        {/*-33-1 Перерисовка страницы!*/ }
+        this._callSubscribe(this._state);
     }
+
+
 }
 
-{/*-33-1 обновление текста в state после ввода на странице, для последующего отображения*/}
-export const PushPostText = (newPostText) => {
-    state.ProfilePage.newPostText = newPostText;
-    rerenderEntireTree(state);
-}
+{/*Отладка store, в консоле просто ввести название*/ }
+window.store = store;
 
-{/*Отладка state, в консоле просто ввести название*/}
-window.state = state;
-
-{/*-32-1 добавление данных функция из BLL*/ }
-export const addPost = () => {
-    let newPost = {
-        id: '3',
-        avatar: '',
-        name: state.ProfilePage.newPostText,
-        likeCount: 0
-    };
-    state.ProfilePage.newPostText = '';
-    state.ProfilePage.postData.push(newPost)
-    {/*-33-1 Перерисовка страницы!*/ }
-    rerenderEntireTree(state);
-}
-
-{/*-35-1 избавление от цикличности*/}
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state
+export default store
 {/*Конец данных!*/ }
