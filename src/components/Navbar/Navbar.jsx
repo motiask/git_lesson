9bca822a2b869cc9ad1a3c4545ff90b28dffe790
Астапчик Матвей <m.astapchik@sellwin.by>
2022-03-29 16:46:36 +0300

    lesson 53

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,6 +15,9 @@ const Navbar = (props) => {
         <NavLink to='/dialogs' className = {navData => navData.isActive ? s.activeLink : s.item}>Messages</NavLink>
       </div>
       <div className={s.item}>
+        <NavLink to='/users' className = {navData => navData.isActive ? s.activeLink : s.item}>Users</NavLink>
+      </div>
+      <div className={s.item}>
         <a>News</a>
       </div>
       <div className={s.item}>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { followActionCreator, setUsersCreator, unfollowActionCreator } from '../../redux/users-reduser';
-import Users from './Users';
+//import Users from './Users';
+import Users from './UsersClass';
 
 let mapStateToProps = (state) => {
     return {
