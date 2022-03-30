65f1f93ad6ded1d320357250b62a8d8ed76d6742
Астапчик Матвей <m.astapchik@sellwin.by>
2022-03-30 09:14:36 +0300

    lesson 56

--- src/components/Users/_not_using_UsersClassAPIComponent.jsx
+++ src/components/Users/_not_using_UsersClassAPIComponent.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { followActionCreator, setCurrentPageCreator, setTotalUsersCountCreator, setUsersCreator, unfollowActionCreator } from '../../redux/users-reduser';
-import Users from './Users';
 import * as axios from 'axios';
+import Users from './Users';
 
-class UsersClassContainer extends React.Component {
+class UsersAPIComponent extends React.Component {
 
     //-54-метод вмонтировать. Используется из react
     componentDidMount() {
@@ -40,35 +38,4 @@ class UsersClassContainer extends React.Component {
 
 }
 
-let mapStateToProps = (state) => {
-    return {
-        usersData: state.usersPage.usersData,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage
-    }
-}
-
-let mapDispatchToProps = (dispatch) => {
-    return {
-        follow: (userId) => {
-            dispatch(followActionCreator(userId))
-        },
-        unfollow: (userId) => {
-            dispatch(unfollowActionCreator(userId))
-        },
-        setUsers: (users) => {
-            dispatch(setUsersCreator(users))
-        },
-        setCurrentPage: (currentPage) => {
-            dispatch(setCurrentPageCreator(currentPage))
-        },
-        setTotalUsersCount: (totalCount) => {
-            dispatch(setTotalUsersCountCreator(totalCount))
-        }
-
-    }
-
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(UsersClassContainer);
+export default UsersAPIComponent
